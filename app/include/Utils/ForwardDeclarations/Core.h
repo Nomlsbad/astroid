7052fb00ab467a0418693e78890143e3b6ee2e62
Yurasov Ilia <iliayurasov@yandex.ru>
2023-12-29 05:27:23 +0300

    Add file for core forward declaration

---
+++ app/include/Utils/ForwardDeclarations/Core.h
@@ -0,0 +1,12 @@
+#ifndef FORWARDDECLARATIONS_H
+#define FORWARDDECLARATIONS_H
+
+namespace TGEngine::Core
+{
+class Actor;
+
+class Level;
+class World;
+} // namespace TGEngine::Core
+
+#endif // FORWARDDECLARATIONS_H
