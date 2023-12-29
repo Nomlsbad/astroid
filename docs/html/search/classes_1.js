2f8a481bbc8ca331eb22577271b34f8afb34f15d
Yurasov Ilia <iliayurasov@yandex.ru>
2023-12-29 22:39:42 +0300

    Dev (#6)
    * Add Actor class
    
    * Add file for declare aliases
    
    * Add file for core forward declaration
    
    * Add World and Level classes.
    Add functional for transfer actors between levels
    
    * Add interface for action
    
    * Add ChangeLevel action
    
    * Remove trash from main
    
    * Create cplus_ci.yml
    
    * Level name (#1)
    
    * Add name field to Level
    
    * Add findLevelByName in World class
    
    * Refactoring getActorLocation with stl algorithms
    
    * Remove on push event (#2)
    
    * Doxygen (#3)
    
    * Add documentation with doxygen
    
    * Create index.html for documentation page
    
    * Doxygen (#5)
    
    * Add documentation with doxygen
    
    * Create index.html for documentation page
    
    * Add doxygen description for Level.h file
    
    * Set FULL_PATH_NAMES NO in Doxyfile

--- docs/html/search/classes_1.js
+++ docs/html/search/classes_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['changelevel_6',['ChangeLevel',['../class_t_g_engine_1_1_gameplay_1_1_change_level.html',1,'TGEngine::Gameplay']]]
+  ['changelevel_7',['ChangeLevel',['../class_t_g_engine_1_1_gameplay_1_1_change_level.html',1,'TGEngine::Gameplay']]]
 ];
--- docs/html/search/classes_4.js
+++ docs/html/search/classes_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['world_9',['World',['../class_t_g_engine_1_1_core_1_1_world.html',1,'TGEngine::Core']]]
+  ['world_10',['World',['../class_t_g_engine_1_1_core_1_1_world.html',1,'TGEngine::Core']]]
 ];
