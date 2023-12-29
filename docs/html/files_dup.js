2699332cf8c53c6b16c26848bf2374c070dac7b6
Yurasov Ilia <iliayurasov@yandex.ru>
2023-12-29 22:14:03 +0300

    Set FULL_PATH_NAMES NO in Doxyfile

--- docs/html/files_dup.js
+++ docs/html/files_dup.js
@@ -1,4 +1,12 @@
 var files_dup =
 [
-    [ "app", "dir_d422163b96683743ed3963d4aac17747.html", "dir_d422163b96683743ed3963d4aac17747" ]
+    [ "Actor.h", "_actor_8h_source.html", null ],
+    [ "ChangeLevel.h", "_change_level_8h_source.html", null ],
+    [ "Core.h", "_core_8h_source.html", null ],
+    [ "CppTypes.h", "_cpp_types_8h_source.html", null ],
+    [ "IAction.h", "_i_action_8h_source.html", null ],
+    [ "Level.h", "_level_8h.html", [
+      [ "Level", "class_t_g_engine_1_1_core_1_1_level.html", "class_t_g_engine_1_1_core_1_1_level" ]
+    ] ],
+    [ "World.h", "_world_8h_source.html", null ]
 ];
