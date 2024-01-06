78ea09dfa224518cfd5d18f6d11788b6ba3a2ee7
Yurasov Ilia <iliayurasov@yandex.ru>
2024-01-06 17:13:01 +0300

    Add documentation for Level

--- app/include/Game/World/Level.h
+++ app/include/Game/World/Level.h
@@ -14,10 +14,21 @@
 namespace TGEngine::Game
 {
 
+/**
+ * @class Level
+ * @brief Storage for game actors.
+ *
+ * Anyone actor is owned by the only ONE level object.
+ * So any pawn or building must be placed in the level.
+ */
 class Level
 {
 public:
 
+    /**
+     * @brief Level constructor
+     * @param name level's string name
+     */
     explicit Level(std::string name);
 
     Level(const Level& level) = delete;
@@ -28,10 +39,29 @@ public:
 
 public:
 
-    [[nodiscard]] std::string getName() const { return name; }
+    /**
+     * @brief Getter for level's name
+     * @return string with level's name
+     */
+    [[nodiscard]]
+    constexpr std::string getName() const
+    {
+        return name;
+    }
 
+    /**
+     * @brief Moves actor from his level to that level
+     * @param actor shared pointer to actor that is being moved
+     */
     void transferActor(const SharedPtr<Actor>& actor);
-    [[nodiscard]] bool isAtLevel(const SharedPtr<Actor>& actor) const;
+
+    /**
+     * @brief Checks does level hold some actor or not
+     * @param actor shared pointer to target actor
+     * @return true if level contains this actor, false in other case
+     */
+    [[nodiscard]]
+    bool isAtLevel(const SharedPtr<Actor>& actor) const;
 
 private:
 
