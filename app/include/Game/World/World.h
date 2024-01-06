aa723d7fc6f9afa96cf82ce9adeb0e1e441385b0
Yurasov Ilia <iliayurasov@yandex.ru>
2024-01-06 17:11:51 +0300

    Add documentation for World

--- app/include/Game/World/World.h
+++ app/include/Game/World/World.h
@@ -1,3 +1,8 @@
+/**
+ * @file World.h
+ * @brief World class declaration.
+ */
+
 #ifndef WORLD_H
 #define WORLD_H
 
@@ -9,10 +14,20 @@
 namespace TGEngine::Game
 {
 
+/**
+ * @class World
+ * @brief Levels storage.
+ *
+ * Singleton class taht contains all game levels
+ */
 class World final
 {
 public:
 
+    /**
+     * @brief Access to world instance.
+     * @return ref to world instance
+     */
     static World& instance();
 
 private:
@@ -21,8 +36,21 @@ private:
 
 public:
 
-    [[nodiscard]] SharedPtr<Level> findLevelByName(const std::string& name) const;
-    [[nodiscard]] SharedPtr<Level> findActorLocation(const SharedPtr<Actor>& actor) const;
+    /**
+     * @brief Try to find level with given name.
+     * @param name level's name
+     * @return shared pointer to first level with given name or nullptr
+     */
+    [[nodiscard]]
+    SharedPtr<Level> findLevelByName(const std::string& name) const;
+
+    /**
+     * @brief Try to find level with given actor.
+     * @param actor shared pointer to target actor
+     * @return shared pointer to level with given actor or nullptr
+     */
+    [[nodiscard]]
+    SharedPtr<Level> findActorLocation(const SharedPtr<Actor>& actor) const;
 
 private:
 
