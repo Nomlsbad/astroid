30d43e163e9500a457d670c373f058eb8a15f8d3
Yurasov Ilia <iliayurasov@yandex.ru>
2024-01-06 17:13:09 +0300

    Add documentation for ChangeLevel

--- app/src/Game/Actions/ChangeLevel.cpp
+++ app/src/Game/Actions/ChangeLevel.cpp
@@ -3,10 +3,12 @@
 #include "Game/World/Level.h"
 
 using TGEngine::Game::Actor;
-using TGEngine::Game::Level;
 using TGEngine::Game::ChangeLevel;
+using TGEngine::Game::Level;
 
-ChangeLevel::ChangeLevel(WeakPtr<Level> level): level(std::move(level)) {}
+ChangeLevel::ChangeLevel(WeakPtr<Level> level)
+    : level(std::move(level))
+{}
 
 void ChangeLevel::doAction(const SharedPtr<Actor>& target) const
 {
