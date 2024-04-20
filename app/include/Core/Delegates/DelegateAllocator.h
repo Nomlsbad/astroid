9ccf1d5aa6d312cf97c01a0c893585ccd4623605
Nomlsbad <iliayurasov@yandex.ru>
2024-04-20 23:32:15 +0300

    Implement allocator for delegates

--- app/include/Core/Delegates/DelegateAllocator.h
+++ app/include/Core/Delegates/DelegateAllocator.h
@@ -1,13 +1,13 @@
 #ifndef DELEGATEALLOCATOR_H
 #define DELEGATEALLOCATOR_H
 
-#include "Core/Memory/StackAllocator.h"
+#include "Core/Memory/StackStorage.h"
 #include "DelegateTypes.h"
 
 namespace TGEngine::Core
 {
 
-template<typename DelegateSignature>
+template<typename DelegateType>
 class DelegateAllocator
 {
 public:
@@ -17,24 +17,40 @@ public:
 
     using Storage = StackStorage<size, align>;
 
-    DelegateAllocator()
-        : storage()
+    using value_type = DelegateType;
+
+    explicit DelegateAllocator(Storage& storage)
+        : storage(storage)
     {}
 
-    using ErasedDelegate = IDelegate<DelegateSignature>*;
-
-    ErasedDelegate allocate(size_t n)
+    DelegateType* allocate(size_t n)
     {
-        if (storage.used() + n < size)
-        {
-            return storage.push(n);
-        }
+       const size_t bytes = n * sizeof(DelegateType);
+       void* ptr = canAllocateInStorage(bytes) ? storage.push(bytes) : std::malloc(bytes);
+       return static_cast<DelegateType*>(ptr);
+    }
 
+    void deallocate(DelegateType* delegate, size_t)
+    {
+        const size_t used = storage.used();
+        hasStorageAllocation(delegate) ? storage.pop(delegate, used) : std::free(delegate);
+    }
+
+    [[nodiscard]]
+    bool canAllocateInStorage(size_t bytes) const
+    {
+        return storage.used() + bytes <= size && storage.isEmpty();
+    }
+
+    [[nodiscard]]
+    bool hasStorageAllocation(DelegateType* delegate) const
+    {
+        return storage.contains(delegate);
     }
 
 private:
 
-    Storage storage;
+    Storage& storage;
 };
 
 } // namespace TGEngine::Core
